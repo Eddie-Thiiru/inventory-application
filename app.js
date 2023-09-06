5d9b2820e05b292dd3012a92df1e0e50c6a0bf85
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-09-06 09:25:51 +0300

    Add MongoDB database using mongoose

--- app.js
+++ app.js
@@ -5,9 +5,21 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const productsRouter = require("./routes/products");
 
 const app = express();
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+// Set up mongoose connection
+const mongoDB =
+  "mongodb+srv://eddiewachaga:AV0KlJvsA3BPgLXW@cluster0.yzd0gvt.mongodb.net/tv_store?retryWrites=true&w=majority";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -20,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
